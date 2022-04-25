f615aeefb26fbc40fc591d1a97682aa83db2d710
Abhi <abhiptl369@gmail.com>
2022-04-25 19:38:44 +0200

    Searchbar_tool using ReactJS

---
+++ src/SearchBar.jsx
@@ -0,0 +1,13 @@
+import {Autocomplete} from "@mui/material";
+
+export default function SearchBar(){
+    return(
+        <div className="search">
+<div className="seachInput">
+    <Autocomplete>
+
+    </Autocomplete>
+</div>
+         </div>    
+         )
+}
